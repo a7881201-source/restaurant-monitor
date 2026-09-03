d151844d465108f9a101d0b94ceb7cba147389e1
a7881201-source <a7881201@gmail.com>
2026-09-03 14:54:34 +0800

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,3 +1,4 @@
+import requests
 import os
 import smtplib
 from email.mime.text import MIMEText
@@ -5,8 +6,20 @@ from email.mime.text import MIMEText
 EMAIL = os.environ["EMAIL_USER"]
 PASSWORD = os.environ["EMAIL_PASSWORD"]
 
-msg = MIMEText("測試通知成功")
-msg["Subject"] = "島語監控測試"
+url = "https://tw.eztable.com/restaurant/17768"
+
+response = requests.get(
+    url,
+    headers={
+        "User-Agent": "Mozilla/5.0"
+    }
+)
+
+html = response.text[:2000]
+
+msg = MIMEText(html)
+
+msg["Subject"] = "EZTABLE測試"
 msg["From"] = EMAIL
 msg["To"] = EMAIL
 
@@ -14,4 +27,4 @@ with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
     smtp.login(EMAIL, PASSWORD)
     smtp.send_message(msg)
 
-print("Mail sent successfully")
+print("Mail Sent")
