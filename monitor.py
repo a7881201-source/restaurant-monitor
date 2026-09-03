911c9922d19dcf8757e69d575faa6418c915fdc8
a7881201-source <a7881201@gmail.com>
2026-09-03 15:14:37 +0800

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -2,6 +2,7 @@ import requests
 import os
 import smtplib
 from email.mime.text import MIMEText
+from bs4 import BeautifulSoup
 
 EMAIL = os.environ["EMAIL_USER"]
 PASSWORD = os.environ["EMAIL_PASSWORD"]
@@ -10,16 +11,28 @@ url = "https://tw.eztable.com/restaurant/17768"
 
 response = requests.get(
     url,
-    headers={
-        "User-Agent": "Mozilla/5.0"
-    }
+    headers={"User-Agent": "Mozilla/5.0"}
 )
 
-html = response.text[:2000]
+soup = BeautifulSoup(response.text, "html.parser")
 
-msg = MIMEText(html)
+result = []
 
-msg["Subject"] = "EZTABLE測試"
+result.append("TITLE:")
+result.append(soup.title.text if soup.title else "No Title")
+
+result.append("\nLINKS:")
+
+for link in soup.find_all("a")[:50]:
+    href = link.get("href")
+    if href:
+        result.append(href)
+
+mail_text = "\n".join(result)
+
+msg = MIMEText(mail_text)
+
+msg["Subject"] = "EZTABLE Links Test"
 msg["From"] = EMAIL
 msg["To"] = EMAIL
 
